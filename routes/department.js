d9c395079acb012cf1ac885772b7b540a0e5eed4
ijc3093 <49662848+ijc3093@users.noreply.github.com>
2021-06-03 14:52:04 -0400

    solved the issue of the master

---
+++ routes/department.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { GetDepartments } = require('../dataAccessLayer/department-controller');
+
+//get all departments
+router.get('/getDepartments', GetDepartments);
+
+module.exports = router;
